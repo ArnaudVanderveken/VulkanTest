1b7ba77765cdfd1b28fb73de6e385c10b0d5ce1f
Arnaud Vanderveken <vanderveken.arnaud@gmail.com>
2023-09-07 10:55:18 +0200

    Creating image views

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -90,6 +90,7 @@ private:
 	std::vector<VkImage> m_swapChainImages{};
 	VkFormat m_swapChainImageFormat{};
 	VkExtent2D m_swapChainExtent{};
+	std::vector<VkImageView> m_swapChainImageViews{};
 
 	// STATIC METHODS
 	static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
@@ -108,7 +109,7 @@ private:
 	}
 
 	// METHODS
-	bool CheckDeviceExtensionSupport(VkPhysicalDevice device)
+	static bool CheckDeviceExtensionSupport(VkPhysicalDevice device)
 	{
 		uint32_t extensionCount;
 		vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, nullptr);
@@ -204,6 +205,10 @@ private:
 
 	void Cleanup() const
 	{
+		for (const auto imageView : m_swapChainImageViews)
+		{
+			vkDestroyImageView(m_device, imageView, nullptr);
+		}
 		vkDestroySwapchainKHR(m_device, m_swapChain, nullptr);
 		if (enableValidationLayers)
 		{
@@ -215,6 +220,37 @@ private:
 
 		glfwDestroyWindow(m_window);
 		glfwTerminate();
+	}
+
+	void CreateImageViews()
+	{
+		m_swapChainImageViews.resize(m_swapChainImages.size());
+
+		for (size_t i = 0; i < m_swapChainImages.size(); ++i)
+		{
+			VkImageViewCreateInfo createInfo{};
+			createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			createInfo.image = m_swapChainImages[i];
+
+			createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			createInfo.format = m_swapChainImageFormat;
+
+			createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+			createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			createInfo.subresourceRange.baseMipLevel = 0;
+			createInfo.subresourceRange.levelCount = 1;
+			createInfo.subresourceRange.baseArrayLayer = 0;
+			createInfo.subresourceRange.layerCount = 1;
+
+			if (vkCreateImageView(m_device, &createInfo, nullptr, &m_swapChainImageViews[i]) != VK_SUCCESS)
+			{
+				throw std::runtime_error("Failed to create image views!");
+			}
+		}
 	}
 
 	void CreateLogicalDevice()
@@ -453,6 +489,7 @@ private:
 		PickPhysicalDevice();
 		CreateLogicalDevice();
 		CreateSwapChain();
+		CreateImageViews();
 	}
 
 	void InitWindow()
