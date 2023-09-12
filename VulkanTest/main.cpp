1e3c561b2c89970491687d02eff122ad532b9d38
Arnaud Vanderveken <vanderveken.arnaud@gmail.com>
2023-09-12 12:06:33 +0200

    Rendering + presentation

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -98,6 +98,9 @@ private:
 	std::vector<VkFramebuffer> m_swapChainFramebuffers{};
 	VkCommandPool m_commandPool{};
 	VkCommandBuffer m_commandBuffer{};
+	VkSemaphore m_imageAvailableSemaphore{};
+	VkSemaphore m_renderFinishedSemaphore{};
+	VkFence m_inFlightFence{};
 
 
 	// DEBUG CALLBACK
@@ -213,6 +216,9 @@ private:
 
 	void Cleanup() const
 	{
+		vkDestroySemaphore(m_device, m_imageAvailableSemaphore, nullptr);
+		vkDestroySemaphore(m_device, m_renderFinishedSemaphore, nullptr);
+		vkDestroyFence(m_device, m_inFlightFence, nullptr);
 		vkDestroyCommandPool(m_device, m_commandPool, nullptr);
 		for (const auto framebuffer : m_swapChainFramebuffers)
 		{
@@ -606,12 +612,22 @@ private:
 		subpass.colorAttachmentCount = 1;
 		subpass.pColorAttachments = &colorAttachmentRef;
 
+		VkSubpassDependency dependency{};
+		dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
+		dependency.dstSubpass = 0;
+		dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+		dependency.srcAccessMask = 0;
+		dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+		dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+
 		VkRenderPassCreateInfo renderPassInfo{};
 		renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
 		renderPassInfo.attachmentCount = 1;
 		renderPassInfo.pAttachments = &colorAttachment;
 		renderPassInfo.subpassCount = 1;
 		renderPassInfo.pSubpasses = &subpass;
+		renderPassInfo.dependencyCount = 1;
+		renderPassInfo.pDependencies = &dependency;
 
 		if (vkCreateRenderPass(m_device, &renderPassInfo, nullptr, &m_renderPass) != VK_SUCCESS)
 		{
@@ -702,6 +718,68 @@ private:
 		m_swapChainExtent = extent;
 	}
 
+	void CreateSyncObjects()
+	{
+		VkSemaphoreCreateInfo semaphoreInfo{};
+		semaphoreInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+
+		VkFenceCreateInfo fenceInfo{};
+		fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+		fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
+
+		if (vkCreateSemaphore(m_device, &semaphoreInfo, nullptr, &m_imageAvailableSemaphore) != VK_SUCCESS
+			|| vkCreateSemaphore(m_device, &semaphoreInfo, nullptr, &m_renderFinishedSemaphore) != VK_SUCCESS
+			|| vkCreateFence(m_device, &fenceInfo, nullptr, &m_inFlightFence) != VK_SUCCESS)
+		{
+			throw std::runtime_error("Failed to create semaphores/fence!");
+		}
+	}
+
+	void DrawFrame()
+	{
+		vkWaitForFences(m_device, 1, &m_inFlightFence, VK_TRUE, UINT64_MAX);
+		vkResetFences(m_device, 1, &m_inFlightFence);
+
+		uint32_t imageIndex;
+		vkAcquireNextImageKHR(m_device, m_swapChain, UINT64_MAX, m_imageAvailableSemaphore, VK_NULL_HANDLE, &imageIndex);
+
+		vkResetCommandBuffer(m_commandBuffer, 0);
+
+		RecordCommandBuffer(m_commandBuffer, imageIndex);
+
+		VkSubmitInfo submitInfo{};
+		submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+
+		const VkSemaphore waitSemaphores[] = { m_imageAvailableSemaphore };
+		constexpr VkPipelineStageFlags waitStages[] = { VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT };
+		submitInfo.waitSemaphoreCount = 1;
+		submitInfo.pWaitSemaphores = waitSemaphores;
+		submitInfo.pWaitDstStageMask = waitStages;
+		submitInfo.commandBufferCount = 1;
+		submitInfo.pCommandBuffers = &m_commandBuffer;
+
+		const VkSemaphore signalSemaphores[] = { m_renderFinishedSemaphore };
+		submitInfo.signalSemaphoreCount = 1;
+		submitInfo.pSignalSemaphores = signalSemaphores;
+
+		if (vkQueueSubmit(m_graphicsQueue, 1, &submitInfo, m_inFlightFence) != VK_SUCCESS) 
+		{
+			throw std::runtime_error("Failed to submit draw command buffer!");
+		}
+
+		VkPresentInfoKHR presentInfo{};
+		presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+		presentInfo.waitSemaphoreCount = 1;
+		presentInfo.pWaitSemaphores = signalSemaphores;
+		const VkSwapchainKHR swapChains[] = { m_swapChain };
+		presentInfo.swapchainCount = 1;
+		presentInfo.pSwapchains = swapChains;
+		presentInfo.pImageIndices = &imageIndex;
+		presentInfo.pResults = nullptr; // Optional
+
+		vkQueuePresentKHR(m_presentQueue, &presentInfo);
+	}
+
 	QueueFamilyIndices FindQueueFamilies(const VkPhysicalDevice& device) const
 	{
 		QueueFamilyIndices indices{};
@@ -769,6 +847,7 @@ private:
 		CreateFramebuffers();
 		CreateCommandPool();
 		CreateCommandBuffer();
+		CreateSyncObjects();
 	}
 
 	void InitWindow()
@@ -781,7 +860,7 @@ private:
 		m_window = glfwCreateWindow(G_WIDTH, G_HEIGHT, "Vulkan", nullptr, nullptr);
 	}
 
-	bool IsDeviceSuitable(VkPhysicalDevice device)
+	bool IsDeviceSuitable(VkPhysicalDevice device) const
 	{
 		// This is an example, can use a scoringsystem too to find the best one.
 		/*VkPhysicalDeviceProperties deviceProperties;
@@ -803,12 +882,15 @@ private:
 		return indices.IsComplete() && swapChainAdequate;
 	}
 
-	void MainLoop() const
+	void MainLoop()
 	{
 		while (!glfwWindowShouldClose(m_window))
 		{
 			glfwPollEvents();
+			DrawFrame();
 		}
+
+		vkDeviceWaitIdle(m_device);
 	}
 
 	void PickPhysicalDevice()
@@ -848,7 +930,7 @@ private:
 		createInfo.pfnUserCallback = DebugCallback;
 	}
 
-	SwapChainSupportDetails QuerySwapChainSupport(VkPhysicalDevice device)
+	SwapChainSupportDetails QuerySwapChainSupport(VkPhysicalDevice device) const
 	{
 		SwapChainSupportDetails details;
 
@@ -894,7 +976,7 @@ private:
 		return buffer;
 	}
 
-	void RecordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex)
+	void RecordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex) const
 	{
 		VkCommandBufferBeginInfo beginInfo{};
 		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
