4b6baa5562915e414e57ac03460a852377f612ee
Arnaud Vanderveken <vanderveken.arnaud@gmail.com>
2023-09-20 09:05:41 +0200

    Moved buffer creation code to helper function

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -315,6 +315,33 @@ private:
 		vkDestroySwapchainKHR(m_device, m_swapChain, nullptr);
 	}
 
+	void CreateBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory) const
+	{
+		VkBufferCreateInfo bufferInfo{};
+		bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+		bufferInfo.size = size;
+		bufferInfo.usage = usage;
+		bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+		if (vkCreateBuffer(m_device, &bufferInfo, nullptr, &buffer) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create buffer!");
+		}
+
+		VkMemoryRequirements memRequirements;
+		vkGetBufferMemoryRequirements(m_device, buffer, &memRequirements);
+
+		VkMemoryAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+		allocInfo.allocationSize = memRequirements.size;
+		allocInfo.memoryTypeIndex = FindMemoryType(memRequirements.memoryTypeBits, properties);
+
+		if (vkAllocateMemory(m_device, &allocInfo, nullptr, &bufferMemory) != VK_SUCCESS) {
+			throw std::runtime_error("failed to allocate buffer memory!");
+		}
+
+		vkBindBufferMemory(m_device, buffer, bufferMemory, 0);
+	}
+
 	void CreateCommandBuffers()
 	{
 		m_commandBuffers.resize(G_MAX_FRAMES_IN_FLIGHT);
@@ -819,35 +846,12 @@ private:
 
 	void CreateVertexBuffer()
 	{
-		VkBufferCreateInfo bufferInfo{};
-		bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-		bufferInfo.size = sizeof(m_vertices[0]) * m_vertices.size();
-		bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
-		bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-
-		if (vkCreateBuffer(m_device, &bufferInfo, nullptr, &m_vertexBuffer) != VK_SUCCESS)
-		{
-			throw std::runtime_error("Failed to create vertex buffer!");
-		}
-
-		VkMemoryRequirements memRequirements;
-		vkGetBufferMemoryRequirements(m_device, m_vertexBuffer, &memRequirements);
-
-		VkMemoryAllocateInfo allocInfo{};
-		allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-		allocInfo.allocationSize = memRequirements.size;
-		allocInfo.memoryTypeIndex = FindMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
-
-		if (vkAllocateMemory(m_device, &allocInfo, nullptr, &m_vertexBufferMemory) != VK_SUCCESS)
-		{
-			throw std::runtime_error("Failed to allocate vertex buffer memory!");
-		}
-
-		vkBindBufferMemory(m_device, m_vertexBuffer, m_vertexBufferMemory, 0);
+		const VkDeviceSize bufferSize = sizeof(m_vertices[0]) * m_vertices.size();
+		CreateBuffer(bufferSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, m_vertexBuffer, m_vertexBufferMemory);
 
 		void* data;
-		vkMapMemory(m_device, m_vertexBufferMemory, 0, bufferInfo.size, 0, &data);
-		memcpy(data, m_vertices.data(), (size_t)bufferInfo.size);
+		vkMapMemory(m_device, m_vertexBufferMemory, 0, bufferSize, 0, &data);
+		memcpy(data, m_vertices.data(), (size_t)bufferSize);
 		vkUnmapMemory(m_device, m_vertexBufferMemory);
 	}
 
@@ -920,7 +924,7 @@ private:
 		m_currentFrame = (m_currentFrame + 1) % G_MAX_FRAMES_IN_FLIGHT;
 	}
 
-	uint32_t FindMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties)
+	uint32_t FindMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties) const
 	{
 		VkPhysicalDeviceMemoryProperties memProperties;
 		vkGetPhysicalDeviceMemoryProperties(m_physicalDevice, &memProperties);
